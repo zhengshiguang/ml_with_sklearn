2d9b0191d87d3da65344a2dffe3a5edc63dac5a7
zhengshiguang <zhengshiguang008@sina.com>
2018-07-20 19:04:32 +0800

    update

--- mnist_classifier/classifier.py
+++ mnist_classifier/classifier.py
@@ -4,4 +4,7 @@ import scipy.io
 
 # Import to a python dictionary
 mat = scipy.io.loadmat('mnist_all.mat')
+print(mat.keys())
 print(mat['train0'])
+print(mat['__header__'])
+print(mat['__version__'])
